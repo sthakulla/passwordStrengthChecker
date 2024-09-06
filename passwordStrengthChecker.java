9138d6ba5b6ec83fbbb0f09315d608d338142286
Subham <39816945+sthakulla@users.noreply.github.com>
2024-09-06 13:39:21 -0700

    initial setup to get input for password

--- passwordStrengthChecker.java
+++ passwordStrengthChecker.java
@@ -2,8 +2,16 @@
 
 public class passwordStrengthChecker {
 
-    
+
     public static void main(String[] args) {
-    
+        String errorMessage = "Please enter password to test: passwordStrengthChecker.java 'Password'";
+
+        if(args.length != 1) {
+            System.out.println(errorMessage);
+            return;
+        }
+        String password = args[0];
+        System.out.println(password);
+
     }
 }
